c3470160af4d1febbd4f5ac44ee9e2274d1409ea
lynw <c54wang@gmail.com>
2022-09-23 23:50:53 -0400

    implement loadTweets and add timeago lib

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
           </header>
           <p>${tweetObject.content.text}</p>
           <footer class="tweet-footer">
-            <section>${tweetObject['created_at']}</section>
+            <section>${timeago.format(tweetObject.created_at)} </section>
             <section>
               <i class="fa-solid fa-flag small-margin blue-icon-colour"></i>
               <i class="fa-solid fa-retweet small-margin blue-icon-colour"></i>
@@ -38,6 +38,23 @@ $(document).ready(function() {
     }
   }
 
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    })
+      .then(function(data) {
+        console.log("loadTweets success:");
+        renderTweets(data);
+      })
+      .catch(function(err) {
+        console.log("loadTweets error:");
+        console.log(err);
+      })
+  };
+
+  loadTweets();
+
   $('.tweet-form').submit(function(event) {
     console.log('Button clicked, performing ajax call...')
     event.preventDefault();
@@ -52,41 +69,12 @@ $(document).ready(function() {
         console.log('Success');
       })
       .catch(function(err){
+        console.log("Submit error:")
         console.log(err);
       })
   });
 
-
-   // Fake data taken from initial-tweets.json
- const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-renderTweets(data);
-
 console.log("Test");
-
 
 });
 
